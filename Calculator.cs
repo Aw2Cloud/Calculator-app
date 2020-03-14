79788bf122aa0744a8bf8c0deb24cab34d8b3507
Aw2Cloud <bayu.st252@gmail.com>
2020-03-14 21:26:31 +0700

    inisialisasi project

--- Calculator.cs
+++ Calculator.cs
@@ -2,16 +2,56 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-		int a = 10;
-		int b = 5;
+		//int a = 10;
+		//int b = 5;
+		int x,y;
 		
-		Console.WriteLine("Hasil Penambahan : {0} + {1} + {2}",a,b,tambah);
-		Console.WriteLine("Hasil Pengurangan : {0} - {1} + {2}",a,b,kurang);
-		Console.WriteLine("Hasil Perkalian : {0} * {1} + {2}",a,b,kali);
-		Console.WriteLine("Hasil Pembagian : {0} / {1} + {2}",a,b,bagi);
+		//Console.WriteLine("Hasil Penambahan : {0} + {1} + {2}",a,b,tambah);
+		//Console.WriteLine("Hasil Pengurangan : {0} - {1} + {2}",a,b,kurang);
+		//Console.WriteLine("Hasil Perkalian : {0} * {1} + {2}",a,b,kali);
+		//Console.WriteLine("Hasil Pembagian : {0} / {1} + {2}",a,b,bagi);
+		Console.WriteLine("Pilih Menu Calculator\n");
+		Console.WriteLine("1. Penambahan");
+		Console.WriteLine("2. Pengurangan");
+		Console.WriteLine("3. Perkalian");
+		Console.WriteLine("4. Pembagian");
+		Console.WriteLine("Input nomor menu : 1-4 : ");
+		Console.ReadKey(x);
 		
-		Console.WriteLine("\n Tekan Sembarang");
+		switch(x){
+			case 1:
+			Console.WriteLine("Penambahan");
+			opsi();
+			Console.WriteLine("Hasil Penambahan : {0} + {1} + {2}",a,b,tambah);
+			break;
+			case 2:
+			Console.WriteLine("Pengurangan");
+			opsi();
+			Console.WriteLine("Hasil Pengurangan : {0} + {1} + {2}",a,b,kurang);
+			break;
+			case 3:
+			Console.WriteLine("Perkalian");
+			opsi();
+			Console.WriteLine("Hasil Perkalian : {0} + {1} + {2}",a,b,kali);
+			break;
+			case 4:
+			Console.WriteLine("Pembagian");
+			opsi();
+			Console.WriteLine("Hasil Pembagian : {0} + {1} + {2}",a,b,bagi);
+			break;
+			default:
+			Console.WriteLine("Maaf Menu Tidak Tersedia");
+		}
+		
+		Console.WriteLine("\n Tekan Sembarang untuk keluar");
 		Console.ReadKey();
+	}
+	static void opsi (){
+		Console.Write("Inputkan Nilai A = ");
+		int a = int.Parse(Console.ReadLine());
+		
+		Console.Write("Inputkan Nilai B = ");
+		int b = int.Parse(Console.ReadLine());
 	}
 	
 	static int tambah(int a, int b){
