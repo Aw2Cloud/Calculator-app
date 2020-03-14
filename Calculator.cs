3e0426f59586a06aa5b3265bced396c759fd9d8f
Aw2Cloud <bayu.st252@gmail.com>
2020-03-14 20:51:42 +0700

    inisialisasi project

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,21 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 5;
 		
+		Console.WriteLine("Hasil Penambahan : {0} + {1} + {2}",a,b,tambah);
+		Console.WriteLine("Hasil Pengurangan : {0} - {1} + {2}",a,b,kurang);
+		
+		Console.WriteLine("\n Tekan Sembarang");
+		Console.ReadKey();
+	}
+	
+	static int tambah(int a, int b){
+		return a+b;
+	}
+	
+	static int kurang(int a, int b){
+		return a-b;
 	}
 }
