098080af757d3751c4b279ed029b15f4fc3fb982
Aw2Cloud <bayu.st252@gmail.com>
2020-03-14 21:06:52 +0700

    inisialisasi project

--- Calculator.cs
+++ Calculator.cs
@@ -7,6 +7,8 @@ class Calculator
 		
 		Console.WriteLine("Hasil Penambahan : {0} + {1} + {2}",a,b,tambah);
 		Console.WriteLine("Hasil Pengurangan : {0} - {1} + {2}",a,b,kurang);
+		Console.WriteLine("Hasil Perkalian : {0} * {1} + {2}",a,b,kali);
+		Console.WriteLine("Hasil Pembagian : {0} / {1} + {2}",a,b,bagi);
 		
 		Console.WriteLine("\n Tekan Sembarang");
 		Console.ReadKey();
@@ -18,5 +20,13 @@ class Calculator
 	
 	static int kurang(int a, int b){
 		return a-b;
+	}
+	
+	static int kali(int a, int b){
+		return a*b;
+	}
+	
+	static int bagi(int a, int b){
+		return a/b;
 	}
 }
